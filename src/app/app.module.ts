4d722ad070463147d03375215aef2ea059462469
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-03-26 10:09:35 +0100

    3 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { BookListComponent } from "./book-list/book-list.component";
 
 @NgModule({
