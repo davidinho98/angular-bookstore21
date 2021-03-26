aa944805c8077000654729624b58ca022966c2d9
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-03-26 09:39:44 +0100

    4 files modified, 4 added

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-book-list',
+  templateUrl: './book-list.component.html'
+})
+export class BookListComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
