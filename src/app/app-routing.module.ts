fa0f22303c761bcb9164b70dfa7fe23149ddec11
davidinho98 <s1810456013@students.fh-hagenberg.at>
2021-04-23 12:30:22 +0200

    6 files modified, 6 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BookDetailsComponent } from './book-details/book-details.component';
-import { BookFormComponent } from './book-form/book-form.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BookDetailsComponent } from "./book-details/book-details.component";
+import { BookFormComponent } from "./book-form/book-form.component";
+import { BookListComponent } from "./book-list/book-list.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch:"full"},
-  {path:'home', component: HomeComponent},
-  {path:'books', component:BookListComponent},
-  {path:'books/:isbn', component:BookDetailsComponent},
-  {path:'admin', component:BookFormComponent},
-  {path:'admin/:isbn', component:BookFormComponent}
-]
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "books", component: BookListComponent },
+  { path: "books/:isbn", component: BookDetailsComponent },
+  { path: "admin", component: BookFormComponent },
+  { path: "admin/:isbn", component: BookFormComponent },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-  
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,34 +1,43 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Book } from '../shared/book';
-import { BookFactory } from '../shared/book-factory';
-import { BookStoreService } from '../shared/book-store.service';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Book } from "../shared/book";
+import { BookFactory } from "../shared/book-factory";
+import { BookStoreService } from "../shared/book-store.service";
+import {ToastrService} from "ngx-toastr";
+import { AuthenticationService } from "../shared/authentication.service";
+
 
 @Component({
-  selector: 'bs-book-details',
-  templateUrl: './book-details.component.html'
+  selector: "bs-book-details",
+  templateUrl: "./book-details.component.html"
 })
 export class BookDetailsComponent implements OnInit {
-  book : Book = BookFactory.empty();
+  book: Book = BookFactory.empty();
 
-  constructor(private bs: BookStoreService, private route:ActivatedRoute, private router:Router) { }
+  constructor(
+    private bs: BookStoreService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService,
+    public authService: AuthenticationService
+  ) {}
 
   ngOnInit() {
     const params = this.route.snapshot.params;
-    this.bs.getSingle(params['isbn']).subscribe(res => this.book = res);
+    this.bs.getSingle(params["isbn"]).subscribe(res => (this.book = res));
   }
 
-  getRating(num:number){
+  getRating(num: number) {
     return new Array(num);
   }
 
   removeBook() {
-    if(confirm("Wollen Sie das Buch wirklich löschen?")){
-      this.bs.remove(this.book.isbn).subscribe(
-        res => {
-          this.router.navigate(['../'],{relativeTo:this.route});
-        }
-      );
+    if (confirm("Wollen Sie das Buch wirklich löschen?")) {
+   
+    this.bs.remove(this.book.isbn).subscribe(res => {
+      this.toastr.success('Buch erfolgreich gelöscht', 'Buch gelöscht!');
+        this.router.navigate(["../"], { relativeTo: this.route });
+      });
     }
   }
 }
